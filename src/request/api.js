17af820f7875ca958dd98242b2e066bb3c011041
beer <huohuo5214@126.com>
2019-07-07 22:31:45 +0800

    上传图片   接口调通

--- src/request/api.js
+++ src/request/api.js
@@ -4,6 +4,8 @@
 import {get, post} from './http'
 
 export const daily_photo_url = '/daily/photo/randomSeq';
+// export const upload_photo_url = 'https://api.hexianwei.com/beer/api/upload/image';
+export const upload_photo_url = 'http://127.0.0.1:8888/beer/api/upload/image';
 
 
 export const dailyPhoto = url => get(url, '');
