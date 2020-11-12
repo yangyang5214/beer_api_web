046a3b2d4151de32e5f608e566c8c4c0da0dfb6b
beer <beer5214@126.com>
2020-11-12 08:30:25 +0800

    add favorite others urls

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -21,13 +21,26 @@
 
       </el-submenu>
 
-      <el-menu-item index="2">
+      <el-submenu index="2">
+        <template slot="title">强烈推荐</template>
+
+        <el-menu-item
+          v-for="nav_item in favorite_nav_list"
+          :key="nav_item.index"
+          :index="nav_item.index"
+        >
+          <a v-bind:href="nav_item.index" target="_blank">{{ nav_item.title }}</a>
+        </el-menu-item>
+
+      </el-submenu>
+
+      <el-menu-item index="3">
         <a
           href="https://www.hexianwei.com"
           target="_blank"
         >个人网站</a>
       </el-menu-item>
-      <el-menu-item index="2">
+      <el-menu-item index="4">
         <a
           href="https://beef.hexianwei.com"
           target="_blank"
@@ -40,24 +53,27 @@
 </template>
 
 <script>
-    export default {
-        name: 'NavMenu',
-        data() {
-            return {
-                nav_list: [
-                    {index: '/tools/dailyPhoto', title: '随机一图'},
-                    {index: '/tools/jrebel', title: 'jrebel 激活'},
-                    {index: '/tools/tips', title: 'tips'},
-                    {index: '/tools/urlparse', title: 'urlparse'},
-                ]
-            }
-        },
-        methods: {
-            handleSelect(key, keyPath) {
-                console.log(key, keyPath);
-            }
-        }
+export default {
+  name: 'NavMenu',
+  data() {
+    return {
+      nav_list: [
+        {index: '/tools/dailyPhoto', title: '随机一图'},
+        {index: '/tools/jrebel', title: 'jrebel 激活'},
+        {index: '/tools/tips', title: 'tips'},
+        {index: '/tools/urlparse', title: 'urlparse'},
+      ],
+      favorite_nav_list: [
+        {index: 'https://crontab.guru/', title: 'crontab'},
+      ]
     }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
 </script>
 
 <style scoped>
